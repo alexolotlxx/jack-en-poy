60cda1afa16efe07f06575a001c55b634439954b
Axelle Legaspi <amlegaspi7@up.edu.ph>
2026-07-10 19:54:05 +0800

    Complete UI for buttons, mini screen, and score trackers
    Integrate functionality to all three buttons, add
    a mini game screen to see current moves and status,
    and an updating score tracker for both user and bot

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,6 @@
 function playGame()
 {
-    let count = 1, humanScore = 0, computerScore = 0;
+    let roundCount = 2, humanScore = 0, computerScore = 0; // WHY 2?????
 
     function getComputerChoice()
     {
@@ -13,93 +13,80 @@ function playGame()
             return "SCISSORS";
     }
 
-    function getHumanChoice()
+    function playRound(humanChoice)
     {
-        let choice = prompt(`ROUND ${count}\nEnter your move:`);
-        return choice.toUpperCase()
-    }
+        let computerChoice = getComputerChoice();
+        
+        // Console
+        console.log(`ROUND ${roundCount}`);
+        console.log(`User current score: ${humanScore}`);
+        console.log(`Computer current score: ${computerScore}`);
+        console.log("\n");
 
-    function playRound(humanChoice, computerChoice)
-    {
-        if (humanChoice === "ROCK" && computerChoice === "SCISSORS")
+        // Interface
+        const roundIndicator = document.querySelector("#round-indicator");
+        const gameScreen = document.querySelector("#game-screen");
+        const upperText = document.querySelector("#upper-text");
+        const bottomText = document.querySelector("#bottom-text");
+        const userScore = document.querySelector("#user-score");
+        const botScore = document.querySelector("#bot-score");
+
+        roundIndicator.textContent = `Round ${roundCount}`;
+        upperText.textContent = `Bot chose ${computerChoice}`;
+
+        // Human Wins Scenario
+        if ((humanChoice === "ROCK" && computerChoice === "SCISSORS") || 
+            (humanChoice === "PAPER" && computerChoice === "ROCK") || 
+            (humanChoice === "SCISSORS" && computerChoice === "PAPER"))
         {
-            console.log("You win!");
+            // Console
+            console.log("User wins");
+            // Game Screen
             humanScore++;
+            bottomText.textContent = "You win!";
+            userScore.textContent = `Your Score: ${humanScore}`;
         }
-        else if (humanChoice === "ROCK" && computerChoice === "PAPER")
+        else if ((humanChoice === "ROCK" && computerChoice === "PAPER") || 
+                (humanChoice === "PAPER" && computerChoice === "SCISSORS") ||
+                (humanChoice === "SCISSORS" && computerChoice === "ROCK"))
         {
-            console.log("You lose!");
+            // Console
+            console.log("User loses");
+            // Game Screen
             computerScore++;
-        }
-        else if (humanChoice === "PAPER" && computerChoice === "ROCK")
-        {
-            console.log("You win!");
-            humanScore++;
-        }
-        else if (humanChoice === "PAPER" && computerChoice === "SCISSORS")
-        {
-            console.log("You lose!");
-            computerScore++;
-        }
-        else if (humanChoice === "SCISSORS" && computerChoice === "PAPER")
-        {
-            console.log("You win!");
-            humanScore++;
-        }
-        else if (humanChoice === "SCISSORS" && computerChoice === "ROCK")
-        {
-            console.log("You lose!");
-            computerScore++;
+            bottomText.textContent = "You lose!";
+            botScore.textContent = `Bot's Score: ${computerScore}`;
         }
         else
         {
-            console.log("It's a tie!");
+            console.log("Tie");
+            bottomText.textContent = "It's a tie!";
         }
-    }
 
-    // MAIN GAME
-    while (count <= 5)
-    {
-        console.log(`ROUND ${count}`);
-
-        // Display current scores
-        console.log(`Your current score: ${humanScore}`);
-        console.log(`Computer's current score: ${computerScore}`);
         console.log("\n");
-
-        // Human picks a move
-        const humanSelection = getHumanChoice();
-        console.log(`You chose ${humanSelection}`);
-
-        // Computer picks a move
-        const computerSelection = getComputerChoice();
-        console.log(`Computer chose ${computerSelection}`);
-
-        // Compares the results
-        playRound(humanSelection, computerSelection);
-        console.log("\n");
-
-        // Updates the round
-        count++;
+        roundCount++;
     }
 
-    // Compares final scores
-    if (humanScore > computerScore)
-    {
-        console.log("You win the game!");
-    }
-    else if (computerScore > humanScore)
-    {
-        console.log("You lose the game! You suck!");
-    }
-    else
-    {
-        console.log("It's a tie!");
-    }
+    // getting the reference for each button from HTML file
+    const rockBtn = document.querySelector("#rock");
+    const paperBtn = document.querySelector("#paper");
+    const scissorsBtn = document.querySelector("#scissors");
+    
+    // function when rock button is clicked
+    rockBtn.addEventListener("click", () => {
+        playRound("ROCK");
+    });
 
-    // Displays final scores
-    console.log(`Your final score: ${humanScore}`);
-    console.log(`Computer's final score: ${computerScore}`);
+    // function when paper button is clicked
+    paperBtn.addEventListener("click", () => {
+        playRound("PAPER");
+    });
+
+    // function when scissors button is clicked
+    scissorsBtn.addEventListener("click", () => {
+        playRound("SCISSORS");
+    });
+
 }
 
 playGame();
